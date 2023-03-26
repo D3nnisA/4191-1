a625c857a09114117fce484495060fe9c0501ea8
Dennis Arnold <2018219459@ub.edu.bz>
2023-03-26 14:01:14 -0600

    completed successful insertions using migrations

--- quiz2/migrations/000001_create_courses_table.up.sql
+++ quiz2/migrations/000001_create_courses_table.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS courses (
 
     course_code  text NOT NULL,  
 	course_title  text NOT NULL,
-	course_credit integer NOT NULL,
-	Version     integer NOT NULL DEFAULT 1 
+	course_credit integer NOT NULL
 
 );
